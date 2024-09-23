1c370c7582028755b524c17734adfecf5697ab64
Jiri Petrlik <jiripetrlik@gmail.com>
2024-09-23 08:55:43 +0200

    RHOAIENG-9532 - Add support for AMD GPU image

--- support/defaults.go
+++ support/defaults.go
@@ -8,4 +8,5 @@ const (
 	CodeFlareSDKVersion = "v0.20.2"
 	RayVersion          = "2.35.0"
 	RayImage            = "quay.io/modh/ray:2.35.0-py39-cu121"
+	RayAMDGpuImage      = "quay.io/rhoai/ray:2.35.0-py39-rocm61-torch24-fa26"
 )
